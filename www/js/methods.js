d7b0ad949cd2d59a2f580ed92bf15ab516bd0748
Timothy Wood <tim@imaginesimplicity.com>
2014-02-25 07:28:47 -0500

    cleanup of comic reader

--- www/js/methods.js
+++ www/js/methods.js
@@ -1,3 +1,6 @@
 // encode string to base64
 function base64_encode(e){var t="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";var n,r,i,s,o,u,a,f,l=0,c=0,h="",p=[];if(!e){return e}do{n=e.charCodeAt(l++);r=e.charCodeAt(l++);i=e.charCodeAt(l++);f=n<<16|r<<8|i;s=f>>18&63;o=f>>12&63;u=f>>6&63;a=f&63;p[c++]=t.charAt(s)+t.charAt(o)+t.charAt(u)+t.charAt(a)}while(l<e.length);h=p.join("");var d=e.length%3;return(d?h.slice(0,d-3):h)+"===".slice(d||3)}
-function getFileExtension(filename) {return (/[.]/.exec(filename)) && /[^.]+$/.exec(filename)[0] || '';}
+// get file extension from string
+function getFileExtension(f){return(/[.]/.exec(f))&&/[^.]+$/.exec(f)[0]||'';}
+// determine if string is an integer
+function isNormalInteger(s){var n=~~Number(s);return String(n)===s&&n>= 0;}
