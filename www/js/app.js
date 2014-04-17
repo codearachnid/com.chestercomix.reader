09f95518ed69010b10fe99285fc645be10b47697
Timothy Wood <tim@imaginesimplicity.com>
2014-04-17 06:30:22 -0400

    eager loading the comix

--- www/js/app.js
+++ www/js/app.js
@@ -89,6 +89,85 @@ $(document).ready(function(){
                       top: "+=-45px",
                       opacity: 0
                     },"fast");
+
+                  var comicZipLocation = 'http://chestercomix.com/app/wp-content/uploads/comix/the-battle-of-little-bighorn.zip',
+                      comicZip = null,
+                      comicManifest = [];
+
+
+
+  var showNav = false;
+  $('.pagination').fadeOut();
+  var slidesInDOM = $('#slides').swiper({
+    createPagination: false,
+    // pagination: '.pagination',
+    // paginationClickable: true,
+    onSlideTouch: function(swiper){
+      $.doTimeout( 'showNav', 100, function(){
+        if( !showNav ){
+          $('.pagination').fadeIn();
+          $('.topNav').animate({
+            top: "0px",
+            opacity: 1
+          },"fast");
+          showNav= true;
+        }
+      });
+    },
+    onTouchEnd: function( swiper ){
+
+      // load swiper
+      if( swiper.slides.length < comicManifest.length && swiper.activeIndex+1 >= swiper.slides.length ){
+        // console.log( swiper.slides.length + ":" + swiper.activeIndex);
+        // console.log( comicManifest );
+        // console.log( 'add slide: ' + comicManifest[ swiper.activeIndex+1 ] );
+        addSlideToReader( comicManifest[ swiper.activeIndex+1 ], comicZip, slidesInDOM );
+      }
+
+
+      $.doTimeout( 'showNav', 2000, function(){
+        if(showNav){
+            $('.pagination').fadeOut();
+            $('.topNav').animate({
+              top: "+=-20px",
+              opacity: 0
+            },"fast");
+            showNav = false;
+        }
+      });
+    }
+  });
+
+
+$.ajax({
+  url: comicZipLocation,
+  beforeSend: function( xhr ) {
+    // console.log('getting zip');
+    xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+  },
+  success: function( data, status, xhr ){
+    // console.log('got zip');
+    comicZip = new JSZip(data);
+    var manifestRAW = comicZip.file("comix.json").asBinary();
+    // console.log( 'listRAW: ' + listRAW);
+    comicManifest = jQuery.parseJSON( manifestRAW );
+    // alert(fileList);
+
+    jQuery.each( comicManifest, function( i, file ) {
+      
+      // append image  
+      addSlideToReader( file, comicZip, slidesInDOM );
+
+        if( i == 3)
+            return false;
+    });
+
+}});
+
+
+
+
+
                 break;
             case 'support':
             $('#deviceProperties').html( 'Device Name: '     + device.name     + '<br />' + 
@@ -127,3 +206,30 @@ $(document).ready(function(){
   });
 
 });
+
+function addSlideToReader( file, bundle, slider, direction ){
+  file = file || false;
+  direction = direction || 'append';
+  if( !file )
+    return false;
+  var tmpFile = "data:";
+  switch(getFileExtension(file).toLowerCase()) {
+      case 'gif' :
+          tmpFile += "image/gif;base64,";
+          break;
+      case 'png' :
+          tmpFile += "image/png;base64,";
+          break;
+      case 'jpg':
+      case 'jpeg':
+          tmpFile += "image/jpeg;base64,";
+          break;
+  }
+  tmpFile += base64_encode( bundle.file( file ).asBinary() );
+  var newSlide = slider.createSlide('<img src="' + tmpFile + '" />');
+  if( direction == 'append' ){
+    newSlide.append();
+  } else {
+    newSlide.prepend();
+  }
+}
