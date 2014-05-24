26cbd6800fc05d6b0d220421b39e908a9bb9f33a
Timothy Wood <tim@imaginesimplicity.com>
2014-05-24 07:19:37 -0400

    reinit doc ready

--- www/js/app.js
+++ www/js/app.js
@@ -89,199 +89,199 @@ var app = {
         console.log('nativePluginErrorHandler: ' + error);
     }
 };
-//
-//$(document).ready(function () {
-//        showSpinner('loading');
-//
-//        // $('click').on("click", function(event){
-//        $(document.body).on('click', 'click', function (event) {
-//            event.preventDefault();
-//
-//            // gotopage
-//            if ($(this).data('gotopage') != null) {
-//                $('.appPage').hide().siblings('#' + $(this).data('gotopage')).fadeIn();
-//                switch ($(this).data('gotopage')) {
-//                case 'reader':
-//                    $('#nav-dashboard').hide();
-//                    $('#nav-reader').show();
-//                    $('nav.navigation-bar').animate({
-//                        top: "0px",
-//                        opacity: 1
-//                    }, "fast").delay(800).animate({
-//                        top: "+=-45px",
-//                        opacity: 0
-//                    }, "fast");
-//
-//                    var comicZipLocation = 'http://chestercomix.com/app/wp-content/uploads/comix/the-battle-of-little-bighorn.zip',
-//                        comicZip = null,
-//                        comicManifest = [];
-//
-//                    // $('#loading').fadeIn();
-//
-//                    var showNav = false;
-//                    var slidesInDOM = $('#slides').swiper({
-//                        createPagination: false,
-//                        // calculateHeight: true,
-//                        centeredSlides: true,
-//                        visibilityFullFit: true,
-//                        // cssWidthAndHeight:true,
-//                        onSlideTouch: function (swiper) {
-//                            $.doTimeout('showNav', 100, function () {
-//                                if (!showNav) {
-//                                    showNav = true;
-//                                    $('nav.navigation-bar').css('z-index', 9999).animate({
-//                                        top: "0px",
-//                                        opacity: 1
-//                                    }, "fast");
-//                                }
-//                            });
-//                        },
-//                        onTouchEnd: function (swiper) {
-//                            $.doTimeout('showNav', 2000, function () {
-//                                if (showNav) {
-//                                    showNav = false;
-//                                    $('nav.navigation-bar').animate({
-//                                        top: "+=-45px",
-//                                        opacity: 0
-//                                    }, "fast").css('z-index', 0);
-//                                }
-//                            });
-//                        },
-//                        onSlideChangeEnd: function (swiper) {
-//                            // load next image
-//                            // comicZip != null &&
-//                            if (swiper.slides.length < comicManifest.length && swiper.activeIndex + 1 >= swiper.slides.length) {
-//                                // addSlideToReader( comicManifest[ swiper.activeIndex+1 ], comicZip, slidesInDOM );
-//                                addSlideToReader(comicManifest[swiper.activeIndex + 1], slidesInDOM);
-//                            }
-//                        }
-//                    });
-//
-//                    // console.log('fetch: ' + $(this).data('id'));
-//                    amplify.request("comixManifest", {
-//                        "ID": $(this).data('id')
-//                    }, function (response) {
-//                        if (response.comix.length > 0) {
-//                            comicManifest = response.comix[0].panels;
-//                            jQuery.each(response.comix[0].panels, function (i, image) {
-//                                addSlideToReader(image, slidesInDOM);
-//                                if (i == 3)
-//                                    return false;
-//                            });
-//                        }
-//                        $('#loading').fadeOut();
-//                        console.log(response);
-//                        // amplify.request( "comixPayload", { "payloadURL": response.comix[0].payload }, function( data ){
-//                        //   // console.log('got zip');
-//                        //   comicZip = new JSZip(data);
-//                        // });
-//                        // $.ajax({
-//                        //   url: response.comix[0].payload,
-//                        //   beforeSend: function( xhr ) {
-//                        //     // console.log('getting zip');
-//                        //     xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
-//                        //   },
-//                        //   success: function( data, status, xhr ){
-//                        //     // console.log('got zip');
-//                        //     comicZip = new JSZip(data);
-//                        //     var manifestRAW = comicZip.file("comix.json").asBinary();
-//                        //     // console.log( 'listRAW: ' + listRAW);
-//                        //     comicManifest = jQuery.parseJSON( manifestRAW );
-//                        //     // alert(fileList);
-//
-//                        //     jQuery.each( comicManifest, function( i, file ) {
-//
-//                        //       // append image
-//                        //       addSlideToReader( file, comicZip, slidesInDOM );
-//
-//                        //         if( i == 3)
-//                        //             return false;
-//                        //     });
-//
-//                        //     $('#loading').fadeOut();
-//
-//                        // }});
-//
-//                    });
-//
-//
-//
-//
-//                    break;
-//                case 'bookshelf':
-//                    $('#nav-dashboard').show();
-//                    $('#nav-reader').hide();
-//                    if ($("#bookshelf-list").is(':empty')) {
-//
-//                        amplify.request("comixManifest", {}, function (response) {
-//                            $('#loading').fadeOut('fast');
-//                            var tiles = [];
-//                            jQuery.each(response.comix, function (i, comix) {
-//                            	if( comix.owned ) {
-//	                                // var gotopage = comix.owned ? 'reader' : 'purchase';
-//	                                var tile = $('<click>').attr('data-gotopage', 'reader').attr('data-id', comix.ID),
-//	                                    wrapper = $('<div>').addClass('tile ol-transparent'),
-//	                                    content = $('<div>').addClass('tile-content'),
-//	                                    img = $('<img>').attr('src', comix.thumb);
-//	                                content.append(img);
-//	                                wrapper.append(content);
-//	                                tile.append(wrapper);
-//	                                $("#bookshelf-list").append(tile);
-//	                            }
-//                            });
-//                        });
-//
-//                    }
-//                    break;
-//                case 'support':
-//                    $('#deviceProperties').html('Device Name: ' + device.name + '<br />' +
-//                        'Device PhoneGap: ' + device.phonegap + '<br />' +
-//                        'Device Platform: ' + device.platform + '<br />' +
-//                        'Device UUID: ' + device.uuid + '<br />' +
-//                        'Device Version: ' + device.version + '<br />');
-//                    break;
-//                case 'signin':
-//                    break;
-//                default:
-//                    // $('nav.navigation-bar').css('z-index',0).animate({
-//                    //         top: "0px",
-//                    //         opacity: 1
-//                    //       },"fast");
-//
-//                    $('.appPage').hide();
-//                    $('#nav-signin').show();
-//                    // $('nav.navigation-bar').animate({
-//                    //     top: "0px",
-//                    //     opacity: 1
-//                    //   },"fast");
-//                    break;
-//                }
-//            }
-//
-//            // checkLoaded();
-//
-//            //    // slideshow
-//            //   if( $(this).data('gotoslide') != null ) {
-//            //         switch( $(this).data('gotoslide') ){
-//            //           case 'first':
-//            //             slide = 0;
-//            //             break;
-//            //           case 'last':
-//            //             slide = slidesInDOM.slides.length-1;
-//            //             break;
-//            //           default:
-//            //             slide = isNormalInteger( $(this).data('gotoslide') ) ? $(this).data('gotoslide') : 0;
-//            //             break;
-//            //         }
-//
-//            //       slidesInDOM.swipeTo(slide);
-//            //   }
-//            // });
-//
-//        });
-//
-//
-//});
+
+$(document).ready(function () {
+        showSpinner('loading');
+
+        // $('click').on("click", function(event){
+        $(document.body).on('click', 'click', function (event) {
+            event.preventDefault();
+
+            // gotopage
+            if ($(this).data('gotopage') != null) {
+                $('.appPage').hide().siblings('#' + $(this).data('gotopage')).fadeIn();
+                switch ($(this).data('gotopage')) {
+                case 'reader':
+                    $('#nav-dashboard').hide();
+                    $('#nav-reader').show();
+                    $('nav.navigation-bar').animate({
+                        top: "0px",
+                        opacity: 1
+                    }, "fast").delay(800).animate({
+                        top: "+=-45px",
+                        opacity: 0
+                    }, "fast");
+
+                    var comicZipLocation = 'http://chestercomix.com/app/wp-content/uploads/comix/the-battle-of-little-bighorn.zip',
+                        comicZip = null,
+                        comicManifest = [];
+
+                    // $('#loading').fadeIn();
+
+                    var showNav = false;
+                    var slidesInDOM = $('#slides').swiper({
+                        createPagination: false,
+                        // calculateHeight: true,
+                        centeredSlides: true,
+                        visibilityFullFit: true,
+                        // cssWidthAndHeight:true,
+                        onSlideTouch: function (swiper) {
+                            $.doTimeout('showNav', 100, function () {
+                                if (!showNav) {
+                                    showNav = true;
+                                    $('nav.navigation-bar').css('z-index', 9999).animate({
+                                        top: "0px",
+                                        opacity: 1
+                                    }, "fast");
+                                }
+                            });
+                        },
+                        onTouchEnd: function (swiper) {
+                            $.doTimeout('showNav', 2000, function () {
+                                if (showNav) {
+                                    showNav = false;
+                                    $('nav.navigation-bar').animate({
+                                        top: "+=-45px",
+                                        opacity: 0
+                                    }, "fast").css('z-index', 0);
+                                }
+                            });
+                        },
+                        onSlideChangeEnd: function (swiper) {
+                            // load next image
+                            // comicZip != null &&
+                            if (swiper.slides.length < comicManifest.length && swiper.activeIndex + 1 >= swiper.slides.length) {
+                                // addSlideToReader( comicManifest[ swiper.activeIndex+1 ], comicZip, slidesInDOM );
+                                addSlideToReader(comicManifest[swiper.activeIndex + 1], slidesInDOM);
+                            }
+                        }
+                    });
+
+                    // console.log('fetch: ' + $(this).data('id'));
+                    amplify.request("comixManifest", {
+                        "ID": $(this).data('id')
+                    }, function (response) {
+                        if (response.comix.length > 0) {
+                            comicManifest = response.comix[0].panels;
+                            jQuery.each(response.comix[0].panels, function (i, image) {
+                                addSlideToReader(image, slidesInDOM);
+                                if (i == 3)
+                                    return false;
+                            });
+                        }
+                        $('#loading').fadeOut();
+                        console.log(response);
+                        // amplify.request( "comixPayload", { "payloadURL": response.comix[0].payload }, function( data ){
+                        //   // console.log('got zip');
+                        //   comicZip = new JSZip(data);
+                        // });
+                        // $.ajax({
+                        //   url: response.comix[0].payload,
+                        //   beforeSend: function( xhr ) {
+                        //     // console.log('getting zip');
+                        //     xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+                        //   },
+                        //   success: function( data, status, xhr ){
+                        //     // console.log('got zip');
+                        //     comicZip = new JSZip(data);
+                        //     var manifestRAW = comicZip.file("comix.json").asBinary();
+                        //     // console.log( 'listRAW: ' + listRAW);
+                        //     comicManifest = jQuery.parseJSON( manifestRAW );
+                        //     // alert(fileList);
+
+                        //     jQuery.each( comicManifest, function( i, file ) {
+
+                        //       // append image
+                        //       addSlideToReader( file, comicZip, slidesInDOM );
+
+                        //         if( i == 3)
+                        //             return false;
+                        //     });
+
+                        //     $('#loading').fadeOut();
+
+                        // }});
+
+                    });
+
+
+
+
+                    break;
+                case 'bookshelf':
+                    $('#nav-dashboard').show();
+                    $('#nav-reader').hide();
+                    if ($("#bookshelf-list").is(':empty')) {
+
+                        amplify.request("comixManifest", {}, function (response) {
+                            $('#loading').fadeOut('fast');
+                            var tiles = [];
+                            jQuery.each(response.comix, function (i, comix) {
+                            	if( comix.owned ) {
+	                                // var gotopage = comix.owned ? 'reader' : 'purchase';
+	                                var tile = $('<click>').attr('data-gotopage', 'reader').attr('data-id', comix.ID),
+	                                    wrapper = $('<div>').addClass('tile ol-transparent'),
+	                                    content = $('<div>').addClass('tile-content'),
+	                                    img = $('<img>').attr('src', comix.thumb);
+	                                content.append(img);
+	                                wrapper.append(content);
+	                                tile.append(wrapper);
+	                                $("#bookshelf-list").append(tile);
+	                            }
+                            });
+                        });
+
+                    }
+                    break;
+                case 'support':
+                    $('#deviceProperties').html('Device Name: ' + device.name + '<br />' +
+                        'Device PhoneGap: ' + device.phonegap + '<br />' +
+                        'Device Platform: ' + device.platform + '<br />' +
+                        'Device UUID: ' + device.uuid + '<br />' +
+                        'Device Version: ' + device.version + '<br />');
+                    break;
+                case 'signin':
+                    break;
+                default:
+                    // $('nav.navigation-bar').css('z-index',0).animate({
+                    //         top: "0px",
+                    //         opacity: 1
+                    //       },"fast");
+
+                    $('.appPage').hide();
+                    $('#nav-signin').show();
+                    // $('nav.navigation-bar').animate({
+                    //     top: "0px",
+                    //     opacity: 1
+                    //   },"fast");
+                    break;
+                }
+            }
+
+            // checkLoaded();
+
+            //    // slideshow
+            //   if( $(this).data('gotoslide') != null ) {
+            //         switch( $(this).data('gotoslide') ){
+            //           case 'first':
+            //             slide = 0;
+            //             break;
+            //           case 'last':
+            //             slide = slidesInDOM.slides.length-1;
+            //             break;
+            //           default:
+            //             slide = isNormalInteger( $(this).data('gotoslide') ) ? $(this).data('gotoslide') : 0;
+            //             break;
+            //         }
+
+            //       slidesInDOM.swipeTo(slide);
+            //   }
+            // });
+
+        });
+
+
+});
 
 
 
