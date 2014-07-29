1928bd72e7dd5b889f8d224a4965888724e48798
Timothy Wood <tim@imaginesimplicity.com>
2014-07-29 07:49:05 -0400

    reintegration of custom stripe checkout

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,67 @@
-var debugMode = false;
+var debugMode = true;
 var UUID = 'f06ca12c760a48bb';
 
-
+var usStates = ko.observableArray([
+    {id:"", name: "Select your state"},
+    {id:"AL",name:"Alabama"},
+    {id:"AK",name: "Alaska"},
+    {id:"AS",name: "American Samoa"},
+    {id:"AZ",name: "Arizona"},
+    {id:"AR",name: "Arkansas"},
+    {id:"CA",name: "California"},
+    {id:"CO",name: "Colorado"},
+    {id:"CT",name: "Connecticut"},
+    {id:"DE",name: "Delaware"},
+    {id:"DC",name: "District Of Columbia"},
+    {id:"FM",name: "Federated States Of Micronesia"},
+    {id:"FL",name: "Florida"},
+    {id:"GA",name: "Georgia"},
+    {id:"GU",name: "Guam"},
+    {id:"HI",name: "Hawaii"},
+    {id:"ID",name: "Idaho"},
+    {id:"IL",name: "Illinois"},
+    {id:"IN",name: "Indiana"},
+    {id:"IA",name: "Iowa"},
+    {id:"KS",name: "Kansas"},
+    {id:"KY",name: "Kentucky"},
+    {id:"LA",name: "Louisiana"},
+    {id:"ME",name: "Maine"},
+    {id:"MH",name: "Marshall Islands"},
+    {id:"MD",name: "Maryland"},
+    {id:"MA",name: "Massachusetts"},
+    {id:"MI",name: "Michigan"},
+    {id:"MN",name: "Minnesota"},
+    {id:"MS",name: "Mississippi"},
+    {id:"MO",name: "Missouri"},
+    {id:"MT",name: "Montana"},
+    {id:"NE",name: "Nebraska"},
+    {id:"NV",name: "Nevada"},
+    {id:"NH",name: "New Hampshire"},
+    {id:"NJ",name: "New Jersey"},
+    {id:"NM",name: "New Mexico"},
+    {id:"NY",name: "New York"},
+    {id:"NC",name: "North Carolina"},
+    {id:"ND",name: "North Dakota"},
+    {id:"MP",name: "Northern Mariana Islands"},
+    {id:"OH",name: "Ohio"},
+    {id:"OK",name: "Oklahoma"},
+    {id:"OR",name: "Oregon"},
+    {id:"PW",name: "Palau"},
+    {id:"PA",name: "Pennsylvania"},
+    {id:"PR",name: "Puerto Rico"},
+    {id:"RI",name: "Rhode Island"},
+    {id:"SC",name: "South Carolina"},
+    {id:"SD",name: "South Dakota"},
+    {id:"TN",name: "Tennessee"},
+    {id:"TX",name: "Texas"},
+    {id:"UT",name: "Utah"},
+    {id:"VT",name: "Vermont"},
+    {id:"VI",name: "Virgin Islands"},
+    {id:"VA",name: "Virginia"},
+    {id:"WA",name: "Washington"},
+    {id:"WV",name: "West Virginia"},
+    {id:"WI",name: "Wisconsin"},
+    {id:"WY",name: "Wyoming"}]);
 
 
 var appFramework = new Framework7();
@@ -25,42 +85,75 @@ $$('.prompt-register').on('click', function () {
     appFramework.closeModal();
 });
 
+var paymentModal = '<div class="row no-gutter"><input type="text" placeholder="Credit Card" name="modal-cc" class="modal-text-input modal-text-input-double" /></div>' +
+        '<div class="row no-gutter"><input type="text" placeholder="MM/YY" name="modal-expires" class="col-50 modal-text-input modal-text-input-double modal-text-input-double-left" />' + 
+        '<input type="text" name="modal-cvc" placeholder="CVC Code" class="col-50 modal-text-input modal-text-input-double modal-text-input-double-right" /></div>';
+var paymentModalTitle = 'Credit Card Details';
+var paymentModalButtons = [
+              {
+                text: 'Purchase',
+                bold: true,
+                onClick: stripeRequestHandler
+              },
+              {
+                text: 'Cancel'
+              }
+            ];
 
-var comixObject = function(id, name, description, thumb, owned){
+
+
+var comixObject = function(id, name, description, thumb, featured, owned){
     owned = owned || false;
     return {
         thumb: ko.observable( thumb ),
         id: ko.observable( id ),
         owned: ko.observable( owned ),
         name: ko.observable( name ),
+        featured: ko.observable( featured ),
         description: ko.observable( description )
     }
 };
 
-var vmComixApplication = {
-    authenticated: ko.observable(),
-    context: new comixObject,
-    comix: new comixObject,
-    manifest: ko.observableArray(),
-    gotoComix: function( data, event ){
-        vmComixApplication.context = data;
-        if( data.owned() == 'true'){
-            console.log(data.owned(), data.id());
-            // photobrowser
-        } else {
-            appMain.loadPage('purchase.html');
-            console.log(vmComixApplication.context.id());
-        }
+// var vmComixApplication = {
+//     authenticated: ko.observable(),
+//     context: new comixObject,
+//     comix: new comixObject,
+//     manifest: ko.observableArray(),
+//     gotoComix: function( data, event ){
+//         vmComixApplication.context = data;
+//         if( data.owned() == 'true'){
+//             console.log(data.owned(), data.id());
+//             // photobrowser
+//         } else {
+//             appMain.loadPage('purchase.html');
+//             console.log(vmComixApplication.context.id());
+//         }
         
-    }
-};
+//     }
+// };
 
 
 
 var context = {
     comix: new comixObject,
     authenticated: ko.observable(false),
-    UUID: ko.observable( UUID )
+    UUID: ko.observable( UUID ),
+    paymentKey: {
+        secret: ko.observable(),
+        publish: ko.observable()
+    },
+    user: {
+        name: ko.observable(''),
+        email: ko.observable(''),
+        password: ko.observable(''),
+        billing_name: ko.observable(''),
+        billing_address: ko.observable(''),
+        billing_address2: ko.observable(''),
+        billing_city: ko.observable(''),
+        billing_state: ko.observable(''),
+        billing_zip: ko.observable('')
+    },
+    purchaseAttempt: ko.observable('')
 };
 var vmComixManifest = {
     manifest: ko.observableArray()
@@ -70,34 +163,43 @@ var vmComixManifest = {
 
 // });
 var vmComixIndex = {
+    applied: false,
     manifest: vmComixManifest.manifest,
-    gotoComix: function( data, event ){
-        context.comix = data;
-        if( data.owned() == 'true'){
-            console.log(data.owned(), data.id());
-            // photobrowser
-        } else {
-            appMain.loadPage('purchase.html');
-        }
-        
-    }
+    gotoComix: gotoComixPage
 };
 var vmAppTopNavigation = {
+    applied: false,
     authenticated: context.authenticated,
     allowBack: ko.observable(false)
 };
+var vmAppSideNavigation = {
+    applied: false,
+    authenticated: context.authenticated,
+    bookshelf: ko.observable()
+};
 var vmPurchase = {
-
+    applied: false,
+    buyComixNow: buyComix
 };
 var vmAccount = {
-
+    applied: false,
+    user: context.user,
+    UUID: context.UUID,
+    states: usStates
 };
 var vmRegister = {
+    applied: false,
     UUID: context.UUID
-}
+};
 
 var vmAppSettings = {
+    applied: false,
     version: ko.observable()
+};
+var vmBookshelf = {
+    applied: false,
+    manifest: vmComixManifest.manifest,
+    gotoComix: gotoComixPage
 };
 
 var chesterComix = {
@@ -115,6 +217,8 @@ var chesterComix = {
         // ko.applyBindings( vmComixApplication );
         ko.applyBindings( vmComixIndex, document.getElementById('comix-index') );
         ko.applyBindings( vmAppTopNavigation, document.getElementById('app-navbar') );
+        ko.applyBindings( vmAppSideNavigation, document.getElementById('app-flyout-panel') );
+
         // temp
         this.onDeviceReady();
     },
@@ -125,22 +229,51 @@ var chesterComix = {
         fetchManifest()
         
         appFramework.onPageInit('index', function (page) {
-            ko.applyBindings( vmComixIndex, document.getElementById('comix-index') );
+            var element = document.getElementById('comix-index');
+            ko.cleanNode(element);
+            ko.applyBindings( vmComixIndex, element );
             fetchManifest();    
         });
         appFramework.onPageInit('bookshelf', function (page) { 
-            
+            var element = document.getElementById('comix-bookshelf');
+            ko.cleanNode(element);
+            ko.applyBindings( vmBookshelf, element );
         });
         appFramework.onPageInit('purchase', function (page) {
             vmAppTopNavigation.allowBack(true);
-            ko.applyBindings( vmPurchase, document.getElementById('comix-purchase') );
+            var element = document.getElementById('comix-purchase');
+            ko.cleanNode(element);
+            ko.applyBindings( vmPurchase, element );
         });
         appFramework.onPageBeforeRemove('purchase',function (page){
             vmAppTopNavigation.allowBack(false);
         });
         appFramework.onPageInit('account', function (page) {
             vmAppTopNavigation.allowBack(true);
-            ko.applyBindings( vmAccount, document.getElementById('app-account') );
+            var element = document.getElementById('app-account');
+            ko.cleanNode(element);
+            ko.applyBindings( vmAccount, element );
+            $$('#app-account-details-form').on('submitted', function (e) {
+                console.log(e.detail.data);
+              var response = JSON.parse(e.detail.data);
+              if( response.status ){
+                appFramework.addNotification({
+                    hold: 3000,
+                    title: 'Success',
+                    message: 'Thank you for updating your account information!',
+                    onClose: function () {
+                        if( context.purchaseAttempt() != '' ) {
+                            // console.log(context.purchaseAttempt())
+                            gotoComixPage( context.purchaseAttempt() );
+                        } else {
+                            appMain.loadPage('index.html');
+                        }
+                    }
+                });
+              } else {
+                appFramework.alert(response.message);
+              }
+            });
         });
         appFramework.onPageBeforeRemove('account',function (page){
             vmAppTopNavigation.allowBack(false);
@@ -148,7 +281,9 @@ var chesterComix = {
         appFramework.onPageInit('settings', function (page) {
         });
         appFramework.onPageInit('register', function (page){
-            ko.applyBindings( vmRegister, document.getElementById('app-register'));
+            var element = document.getElementById('app-register');
+            ko.cleanNode(element);
+            ko.applyBindings( vmRegister, element );
             $$('#app-register-form').on('submitted', function (e) {
               var response = JSON.parse(e.detail.data);
               if( response.status ){
@@ -193,6 +328,25 @@ var chesterComix = {
         amplify.request('userAuth',{ UUID: context.UUID() }, function(response){
             if( response.status ) {
                 context.authenticated(true);
+
+
+                amplify.request('userContext',{ UUID: context.UUID() }, function(newContext){
+                    context.user.name( newContext.user.name );
+                    context.user.email( newContext.user.email );
+                    context.user.billing_name( newContext.user.billing_name );
+                    context.user.billing_address( newContext.user.billing_address );
+                    context.user.billing_address2( newContext.user.billing_address2 );
+                    context.user.billing_city( newContext.user.billing_city );
+                    context.user.billing_state( newContext.user.billing_state );
+                    context.user.billing_zip( newContext.user.billing_zip );
+                    // console.log(newContext);
+                });
+
+                amplify.request('getPaymentKey',{ UUID: context.UUID() }, function(newContext){
+                    context.paymentKey.secret( newContext.paymentKey.secret );
+                    context.paymentKey.publish( newContext.paymentKey.publish );
+                });
+
                 appFramework.hidePreloader();
                 appFramework.closeModal();
                 appFramework.addNotification({
@@ -200,21 +354,27 @@ var chesterComix = {
                     title: 'Login successful',
                     message: 'You are logged in successfully. Thank you for supporting Chester Comix!',
                 });
+
+
             } else {
                 appFramework.hidePreloader();
             }
         });
     },
     bindRequests: function(){
-        var comixManifestUrl = !debugMode ? "http://www.chestercomix.com/app/api/comix/" : "js/data/manifest.json";
+
         amplify.request.define("comixManifest", "ajax", {
-            url: comixManifestUrl,
+            url: "http://www.chestercomix.com/app/api/comix/",
             dataType: "json",
-            // beforeSend: function (_xhr, _ajaxSettings) {
-            //     appFramework.showPreloader('loading...');
-            // },
             type: "POST",
-            cache: "persist"
+            cache: debugMode ? false : "persist"
+        });
+
+        amplify.request.define("comixRead", "ajax", {
+            url: "http://www.chestercomix.com/app/api/comix/",
+            dataType: "json",
+            type: "POST",
+            cache: debugMode ? false : "persist"
         });
 
         amplify.request.define("comixPayload", "ajax", {
@@ -225,14 +385,28 @@ var chesterComix = {
                 _ajaxSettings.url = decodeURIComponent(_ajaxSettings.data).replace('payloadURL=', '');
             },
             type: "POST",
-            cache: "persist"
+            cache: debugMode ? false : "persist"
         });
 
         amplify.request.define("userAuth", "ajax", {
-            url: "http://www.chestercomix.com/app/api/auth-user/",
+            url: "http://www.chestercomix.com/app/api/user-user/",
             dataType: "json",
             type: "POST",
-            cache: "persist"
+            cache: debugMode ? false : "persist"
+        });
+
+        amplify.request.define("userContext", "ajax", {
+            url: "http://www.chestercomix.com/app/api/user-context/",
+            dataType: "json",
+            type: "POST",
+            cache: debugMode ? false : "persist"
+        });
+
+        amplify.request.define("getPaymentKey", "ajax", {
+            url: "http://www.chestercomix.com/app/api/payment-key/",
+            dataType: "json",
+            type: "POST",
+            cache: debugMode ? false : "persist"
         });
     }
 };
@@ -241,15 +415,22 @@ chesterComix.init();
 
 
 function fetchManifest(){
-    amplify.request("comixManifest", {}, function (response) {
+    var bookshelf = 0;
+    amplify.request("comixManifest", { UUID: context.UUID() }, function (response) {
+        // console.log(response);
         jQuery.each(response.comix, function (i, comix) {
             var comixItem = new comixObject(
                 comix.ID,
                 comix.name,
                 comix.description,
                 comix.thumb,
+                comix.featured,
                 comix.owned
                 ) ;
+            if( comix.owned.toString() == 'true' ){
+                bookshelf++;
+                vmAppSideNavigation.bookshelf( bookshelf );
+            }
             var foundItem = ko.utils.arrayFirst(vmComixManifest.manifest(), function(existingItem) {
                     return existingItem.id == comixItem.id;
                 });
@@ -260,52 +441,77 @@ function fetchManifest(){
     });
 }
 
+function gotoComixPage( data, event ){
+        context.comix = data;
+        if( data.owned() == 'true'){
+            // console.log(data);
+            amplify.request("comixManifest", { UUID: context.UUID(), ID: data.id() }, function (response) {
+                console.log(response);
+                var myPhotoBrowserStandalone = appFramework.photoBrowser({
+                    photos : response.comix[0].panels
+                });
+                myPhotoBrowserStandalone.open();
+            });
+        } else {
+            appMain.loadPage('purchase.html');
+        }
+}
 
-
-
-
-
-// Add view
-
-
-
-
-// $$('.open-preloader-title').on('click', function () {
-//     myApp.showPreloader('Custom Title')
-//     setTimeout(function () {
-//         myApp.hidePreloader();
-//     }, 2000);
-// });
-
-// myApp.onPageBeforeInit('index',function (page) {
-//     alert('function (page)');
-// });
-
-// Callbacks to run specific code for specific pages, for example for About page:
-// myApp.onPageInit('index', function (page) {
-//     myApp.showPreloader('Custom Title');
-//     // myApp.showIndicator();
-//     // myApp.showPreloader('Checking credentials');
-
-//     // $$('.open-preloader').on('click', function () {
-//     //     myApp.showPreloader();
-//     //     setTimeout(function () {
-//     //         myApp.hidePreloader();
-//     //     }, 2000);
-//     // });
-
-// });
-
-// myApp.onPageInit('login-screen', function (page) {
-//   var pageContainer = $$(page.container);
-//   pageContainer.find('.list-button').on('click', function () {
-//     var username = pageContainer.find('input[name="username"]').val();
-//     var password = pageContainer.find('input[name="password"]').val();
-//     // Handle username and password
-//     myApp.alert('Username: ' + username + ', Password: ' + password, function () {
-//       mainView.goBack();
-//     });
-//   });
-// }); 
-
-
+function buyComix( data, event ){
+    if( context.user.billing_zip() != '' ) {
+        console.log(context.user);
+        appFramework.modal({
+            title: paymentModalTitle,
+            text: paymentModal,
+            buttons: paymentModalButtons
+            });
+        
+    } else {
+        appMain.loadPage('account.html');
+        appFramework.addNotification({
+            hold: 1500,
+            title: 'Missing information',
+            message: 'Please fill in your billing details before purchasing',
+            onClose: function(){
+                context.purchaseAttempt( data );
+                appFramework.showTab('#account-billing-info');        
+            }
+        });
+        
+    }
+}
+function stripeRequestHandler (modal, index) {
+    // var cc = $(modal).find('.modal-text-input[name="modal-cc"]').val();
+    // var cvc = $(modal).find('.modal-text-input[name="modal-cvc"]').val();
+    var expires = $(modal).find('.modal-text-input[name="modal-expires"]').val().split("/");
+    var expMo = expires[0];
+    var expYr = "20" + expires[1];
+    // appFramework.alert( cc + " " + cvc + " " + expires);
+    Stripe.setPublishableKey(context.paymentKey.publish());
+    Stripe.card.createToken({
+      number: $(modal).find('.modal-text-input[name="modal-cc"]').val(),
+      cvc: $(modal).find('.modal-text-input[name="modal-cvc"]').val(),
+      exp_month: expMo,
+      exp_year: expYr
+    }, stripeResponseHandler);
+  // appFramework.alert(context.paymentKey.publish());
+}
+function stripeResponseHandler(status, response){
+  if (response.error) {
+    // Show the errors on the form
+    $form.find('.payment-errors').text(response.error.message);
+    $form.find('button').prop('disabled', false);
+  } else {
+    // response contains id and card, which contains additional card details
+    var token = response.id;
+    // Insert the token into the form so it gets submitted to the server
+    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+    // and submit
+    $form.get(0).submit();
+  }
+  appFramework.modal({
+    title: paymentModalTitle,
+    text: paymentModal,
+    buttons: paymentModalButtons
+    });
+}
