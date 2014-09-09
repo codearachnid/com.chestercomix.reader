5ab1b8784544a31403463df8a8c746f148c4d10f
Timothy Wood <tim@imaginesimplicity.com>
2014-09-09 16:45:54 -0400

    debugging

--- www/js/app.js
+++ www/js/app.js
@@ -396,7 +396,8 @@ var chesterComix = {
         if( typeof device != 'undefined' )
             console.log(device);
 
-        if( deviceMode &&  typeof device != 'undefined' ) {
+        if( deviceMode && typeof device != 'undefined' ) {
+            alert(device.cordova + "|" + device.platform + "|" + device.version + "|" + device.uuid);
             context.UUID( device.uuid );
         } else {
             context.UUID( 'testbrowser' );
